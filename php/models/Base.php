80e29d677447757be4f0352ed1db68e59f78aaa7
Erik August Johnson <erik@erikaugust.com>
2016-11-17 20:16:59 +0000

    Refactors Base class, and test - for new Database, Cache design

--- php/models/Base.php
+++ php/models/Base.php
@@ -1,65 +1,21 @@
 <?php
 namespace models;
-use PDO;
 
-/* PHANG - Base class
- * Class provides database and caching connections
- *
- *
- *
+/**
+ * Class Base
+ * @package models
+ * @author Erik August Johnson <erik@erikaugust.com>
  */
 
 class Base {
 
     protected $db;
-	public $memcache;
-	public $redis;
+	protected $cache;
 
-    public function __construct($dbset = 'default') {
+    public function __construct($database = 'default') {
 
-		// Setups
-		$this->db = self::dbSetup($dbset);
-		$this->memcache = self::memcacheSetup();
-		$this->redis = self::redisSetup();
-	}
+		$this->database = new Database($database);
+        $this->cache = new Cache();
 
-	public static function dbSetup($dbset = 'default') {
-		// Database provider setup:
-        if (!$settings = parse_ini_file(DB_SETTINGS_DIR, TRUE)) 
-        throw new exception('Unable to open ' . DB_SETTINGS_DIR . '.');
-
-        // Arrange database setup variables from settings:
-        $db_dns = $settings[$dbset]['driver'] .
-        ':host=' . $settings[$dbset]['host'] .
-        ((!empty($settings[$dbset]['port'])) ? (';port=' . $settings[$dbset]['port']) : '') .
-        ';dbname=' . $settings[$dbset]['dbname'];
-        $db_username = $settings[$dbset]['username'];
-        $db_password = $settings[$dbset]['password'];
-		
-		$db = new PDO($db_dns, $db_username, $db_password);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // Sets database timeout to 5 seconds:
-        $db->setAttribute(PDO::ATTR_TIMEOUT, 5);
-		return $db;
-	}
-
-	public static function redisSetup() {
-        $redis = new \Redis() or die("Cannot load redis module.");
-        $redis->connect('127.0.0.1');
-        return $redis;
-    }
-
-	public static function memcacheSetup() {
-		// Memcache provider setup:
-        if (MEMCACHE_LIBRARY == 'memcache') {
-            $memcache = new \Memcache;
-        } else if (MEMCACHE_LIBRARY == 'memcached') {
-            $memcache = new \Memcached;
-        }
-        if (!$memcache->connect(MEMCACHE_HOST, MEMCACHE_PORT)) 
-		throw new exception('Unable to connect to Memcache');       
-		
-		return $memcache;
 	}
 }
--- php/models/Test/BaseTest.php
+++ php/models/Test/BaseTest.php
@@ -2,14 +2,39 @@
 
 namespace models;
 
+/**
+ * Class BaseTest
+ * @package models
+ */
+
 class BaseTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * PHPUnit Tests
+     *
+     * Assertions: https://phpunit.de/manual/current/en/appendixes.assertions.html
+     *
+     * Running a single test:
+     * phpunit --filter {TestMethodName}
+     * OR
+     * php/vendor/bin/phpunit --filter {TestMethodName}
+     *
+     */
+
+    /**
+     * @name testDatabaseConnection
+     * @description Tests database connection
+     */
 	public function testDatabaseConnection() {
-        $this->assertObjectHasAttribute('db', new Base);
+        $this->assertObjectHasAttribute('database', new Base);
     }
 
-    public function testMemcacheConnection() {
-        $this->assertObjectHasAttribute('memcache', new Base);
+    /**
+     * @name testCacheConnection
+     * @description Tests cache connections (Memcache, Redis)
+     */
+    public function testCacheConnection() {
+        $this->assertObjectHasAttribute('cache', new Base);
     }
 
 }
