956aa350cfcd20b3036be77ea021ef7f85f3d35f
Erik August Johnson <erik@erikaugust.com>
2016-11-18 20:54:30 +0000

    Completes basic implmentation of CLI 'test' command

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 #!/usr/bin/env node
 var program = require('commander');
 var chalk = require('chalk');
+var exec = require('child_process').exec;
+
+var version = '0.0.3';
 
 program
-  .arguments('about')
-  .action(function(file) {
-    console.log(
-     chalk.bold.red('Cronic (0.0.1)') 
-    );
-  })
-  .parse(process.argv);
+  .version(version)
+  .arguments('<cmd> [env]')
+  .action(function (cmd, env) {
+    cmdValue = cmd;
+    envValue = env;
+  });
+
+
+program.parse(process.argv);
+
+if (typeof cmdValue === 'undefined') {
+  about();
+  process.exit(1);
+}
+
+if (cmdValue == 'test') {
+  test(envValue);
+}
+
+if (cmdValue == 'about') {
+  about();
+}
+
+
+/**
+ * Methods
+ */
+
+/**
+ * @name about
+ * @description
+ */
+function about() {
+  console.log(
+    chalk.bold.red('Cronic (' + version + ')')
+  );
+  process.exit(1);
+}
+
+/**
+ * @name test
+ * @description Run all PHPUnit tests, or a single test, if specified
+ */
+function test(envValue) {
+
+  var singleTest = typeof envValue !== 'undefined' ? '--filter {'+envValue+'}' : '';
+
+  exec('php/vendor/bin/phpunit ' + singleTest, function(error, stdout, stderr) {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+    console.log(stdout);
+    console.log(stderr);
+    process.exit(1);
+  });
+}
